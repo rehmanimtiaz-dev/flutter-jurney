bf1a32edc5a87974437e2d813e344f22bccc77a1
rehmanimtaz-dev <rehmanimtiaz100@gmail.com>
2026-01-05 20:16:15 +0500

    Initial Flutter project commit

---
+++ bin/dart.dart
@@ -0,0 +1,5 @@
+import 'package:dart/day1.dart';
+
+void main(){
+  day1();
+}
---
+++ lib/day1.dart
@@ -0,0 +1,15 @@
+void day1() {
+  String name = "Rehman Imtiaz";
+  double currrentSalery = 12000.00;
+  double target = 10000000.00;
+
+  bool Milion  = true;
+
+  print('My name is $name');
+
+  print('current SalleryIs  ${currrentSalery}');
+  print('My target sallery is $target');
+
+  print("Do i am Sucess $Milion");
+
+}
